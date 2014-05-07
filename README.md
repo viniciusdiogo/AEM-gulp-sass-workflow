29d03b9f3690f8b40ded36f7be9e84656b06d85b
Chris Ozols <chris.v.ozols@gmail.com>
2014-05-07 16:58:08 -0500

    updates

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ AEM-gulp-sass-workflow
 
 An "alternate" workflow for front end development for users of Adobe Experience Manager.  Features Gulp (node), SASS, and some weirdness.  Just getting started!  More instructions to follow.
 
-Installation instructions:
+### Installation instructions: ###
 
-npm install gulp gulp-ruby-sass gulp-minify-css gulp-jshint gulp-uglify gulp-autoprefixer gulp-rename gulp-clean gulp-notify gulp-cache gulp-livereload gulp-shell --save-dev
+>$ npm install gulp gulp-ruby-sass gulp-minify-css gulp-jshint gulp-uglify gulp-autoprefixer gulp-rename gulp-clean gulp-notify gulp-cache gulp-livereload gulp-shell --save-dev gulp-compass
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,59 @@
 var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minifycss = require('gulp-minify-css'),
-	jshint = require('gulp-jshint'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'),
-	clean = require('gulp-clean'),
-	concat = require('gulp-concat'),
-	notify = require('gulp-notify'),
-	cache = require('gulp-cache'),
-	livereload = require('gulp-livereload'),
-	shell = require('gulp-shell');
-
+	sass = require('gulp-ruby-sass')
+	// autoprefixer = require('gulp-autoprefixer'),
+	// minifycss = require('gulp-minify-css'),
+	// jshint = require('gulp-jshint'),
+	// uglify = require('gulp-uglify'),
+	// rename = require('gulp-rename'),
+	// clean = require('gulp-clean'),
+	// concat = require('gulp-concat'),
+	// cache = require('gulp-cache'),
+	// livereload = require('gulp-livereload'),
+	// shell = require('gulp-shell'),
+	// nd = require('gulp-nodemon'),
+	// watch = require('gulp-watch'),
+	// filter = require('gulp-filter')
+	;
+	
 var http = require('http'),
 	fs = require('fs');
 
 var repo = '/Users/cozo002/workspace/svu-banners-cq5/components/src/main/content/jcr_root/etc/designs/svubanners';
+
+var banner;
+var siteSection;
+
+gulp.task('styles', function() {
+	return gulp.src(repo + '/' + banner + '/css/' + banner + '-' + siteSection + '.scss')
+		.pipe(sass({
+			style: 'expanded',
+			sourcemap: true
+		}))
+		// .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		// .pipe(rename({suffix: '.min'}))
+		// .pipe(minifycss())
+		.pipe(gulp.dest(repo + '/' + banner + '/css'));
+});
+
+gulp.task('default', ['server', 'watch-styles']);
+
+gulp.task('watch-styles', function () {
+	gulp.watch(repo + '/**/*.scss', function (event) {
+		var path = event.path.split('\\');
+		banner = path[path.length - 3];
+		siteSection = path[path.length - 1].split('.')[0];
+		gulp.run('styles');
+	});
+});
+
+// gulp.task('watch', function() {
+// 	var server = livereload();
+// 	gulp.watch(['styles']).on('change', function(file) {
+// 		server.changed(file.path);
+// 	});
+// });
+
+// curl <http path> -Ok
 
 gulp.task('server', function () {
 	return fs.readFile('dev/index.html', function (err, html) {
@@ -26,52 +64,11 @@ gulp.task('server', function () {
 		http.createServer(function(req, res) {
 			console.log('req made');
 			if (req.url.length === 1) {
-				res.writeHeader(200, {"Content-Type": "text/html"});  
+				res.writeHeader(200, {"Content-Type": "text/html", "Last-Modified": new Date()});  
 				res.write(html);
 				res.end();
 				console.log('response written');
 			}
 		}).listen(8080);
 	});
-})
-
-gulp.task('styles', function() {
-	return gulp.src(repo + '**/*.scss')
-		.pipe(sass({ style: 'expanded' }))
-		// .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-		.pipe(gulp.dest('styles'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('styles'))
-		.pipe(notify({ message: 'Styles task complete' }));
 });
-
-gulp.task('vault', ['styles'], function () {
-	// return gulp.src(repo)
-	// .pipe(shell(['vlt ci *'], {cwd: repo}));
-});
-
-// gulp.task('scripts', function() {
-// 	return gulp.src('scripts/*.js')
-// 		.pipe(jshint())
-// 		.pipe(jshint.reporter('default'))
-// 		.pipe(notify({ message: 'Scripts task complete' }));
-// });
-
-
-gulp.task('default', ['server']);
-
-
-gulp.task('watch', function () {
-	// gulp.watch(repo, ['styles']);
-	// gulp.watch('scripts/*.js', ['scripts']);
-});
-
-// gulp.task('watch', function() {
-// 	var server = livereload();
-// 	gulp.watch(['styles']).on('change', function(file) {
-// 		server.changed(file.path);
-// 	});
-// });
-
-// curl <http path> -O
