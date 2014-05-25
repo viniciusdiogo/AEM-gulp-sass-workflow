1654921da10b87591932b8a07ad2ac0548b2ea47
Chris Ozols <chris.v.ozols@gmail.com>
2014-05-25 17:45:22 -0500

    Clean up effort

--- gulpfile.js
+++ gulpfile.js
@@ -38,53 +38,9 @@ gulp.task('styles', function() {
 		.pipe(gulp.dest(repo + '/' + banner + '/css'));
 });
 
-// gulp.task('rename', function () {
-// 	return gulp.src(repo + '../../../content/svubanners/**/original')
-// });
-
-// {base: '../../../content/svubanners/' + banner})
-
-gulp.task('rename-step-1', function () {
-
-	// return gulp.src('/Users/cozo002/workspace/svu-banners-cq5/components/src/main/content/jcr_root/content/dam/svubanners/cub/en/default_cub.png/_jcr_content/renditions/original')
-	return gulp.src('/Users/cozo002/workspace/svu-banners-cq5/components/src/main/content/jcr_root/content/dam/svubanners/cub/**/original')	
-		.pipe(rename(function (path) {
-			var folders = path.dirname.split('\\');
-			var fileName;
-			var dir;
-			for (var i = 0; i < folders.length; i++) {
-				if (/png|gif|jpg|jpeg$/.test(folders[i])) {
-					fileName = folders[i];
-				}
-			}
-			if (fileName) {
-				// console.log(path.dirname.substr(0, path.dirname.length - 24 - fileName.length));
-				// console.log(path.dirname);
-				console.log(path);
-				dir = path.dirname.substr(0, path.dirname.length - 24 - fileName.length);
-				path.dirname = dir;
-				path.basename = fileName.split('.')[0] + 'TEMP';
-				path.extname = '.' + fileName.split('.')[1];
-				console.log(path);
-			}
-		}))
-		.pipe(gulp.dest('./'));
-});
-
-gulp.task('delete-img-dirs', ['rename-step-1'], function () {
-	
-});
-
-gulp.task('rename', ['delete-img-dirs'], function () {
-	
-});
-
 // gulp.task('default', ['server', 'styles', 'watch']);
-gulp.task('default', ['rename']);
+gulp.task('default', ['server']);
 
-gulp.task('retarget-css', ['styles'], function () {
-	console.log('retarget-css fired');
-});
 
 gulp.task('watch', function () {
 	gulp.watch(repo + '/**/*.scss', function (event) {
@@ -95,7 +51,6 @@ gulp.task('watch', function () {
 	});
 	gulp.watch(repo + '/shoppers/css/shoppers-homepage.css', notifyLivereload);
 });
-
 
 gulp.task('server', function () {
 	fs.readFile('./index.html', 'utf8', function (err, data) {
